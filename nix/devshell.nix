dc24091620fa10687afed559524bf960d92a08bd
Kevin Bungeneers <git@bungerous.be>
2024-04-23 22:21:26 +0200

    Added postgresql service

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -8,6 +8,8 @@ let
     "symfonix.localhost"
   ];
 
+  databaseName = "symfonix";
+
   hash = builtins.hashString "sha256" domainList;
 
   caddyFile = pkgs.writeTextFile {
@@ -23,6 +25,24 @@ let
       }
     '';
   };
+
+  startCaddy = pkgs.writeShellScript "start-caddy.sh" ''
+    mkcert -CAROOT
+
+    if [[ ! -f $DEVSHELL_STATE_DIR/data/mkcert/hash || "$(cat $DEVSHELL_STATE_DIR/data/mkcert/hash)" != "${hash}" ]]; then
+      mkdir -p $DEVSHELL_STATE_DIR/data/mkcert
+
+      echo "${hash}" > $DEVSHELL_STATE_DIR/data/mkcert/hash
+
+      pushd $DEVSHELL_STATE_DIR/data/mkcert > /dev/null
+
+      PATH="${pkgs.nssTools}/bin:$PATH" ${pkgs.mkcert}/bin/mkcert ${domainList} 2> /dev/null
+
+      popd > /dev/null
+    fi
+
+    XDG_DATA_HOME=$DEVSHELL_STATE_DIR/data XDG_CONFIG_HOME=$DEVSHELL_STATE_DIR/config exec ${pkgs.caddy}/bin/caddy run --config ${caddyFile} --adapter caddyfile
+  '';
 
   fpmConfig = pkgs.writeTextFile {
     name = "php-fpm.conf";
@@ -50,23 +70,83 @@ let
     exec ${php.dev}/bin/php-fpm -y ${fpmConfig}
   '';
 
-  startCaddy = pkgs.writeShellScript "start-caddy.sh" ''
-    mkcert -CAROOT
+  postgresConf = pkgs.writeTextFile {
+    name = "postgresql.conf";
+    text = ''
+      listen_addresses = '127.0.0.1'
+      port = 5432
+    '';
+  };
 
-    if [[ ! -f $DEVSHELL_STATE_DIR/data/mkcert/hash || "$(cat $DEVSHELL_STATE_DIR/data/mkcert/hash)" != "${hash}" ]]; then
-      mkdir -p $DEVSHELL_STATE_DIR/data/mkcert
+  setupInitialDatabases = ''
+    # Create initial databases
+    dbAlreadyExists="$(
+      echo "SELECT 1 as exists FROM pg_database WHERE datname = '${databaseName}';" | \
+      psql --dbname postgres | \
+      ${pkgs.gnugrep}/bin/grep -c 'exists = "1"' || true
+    )"
+    echo $dbAlreadyExists
+    if [ 1 -ne "$dbAlreadyExists" ]; then
+      echo "Creating database: ${databaseName}"
+      echo 'create database "${databaseName}";' | psql --dbname postgres
+    fi
+  '';
 
-      echo "${hash}" > $DEVSHELL_STATE_DIR/data/mkcert/hash
+  setupPostgres = pkgs.writeShellScriptBin "setup-postgres" ''
+    set -euo pipefail
+    export PATH=${pkgs.postgresql_16}/bin:${pkgs.coreutils}/bin
 
-      pushd $DEVSHELL_STATE_DIR/data/mkcert > /dev/null
-
-      PATH="${pkgs.nssTools}/bin:$PATH" ${pkgs.mkcert}/bin/mkcert ${domainList} 2> /dev/null
-
-      popd > /dev/null
+    POSTGRES_RUN_INITIAL_SCRIPT="false"
+    if [[ ! -d "$PGDATA" ]]; then
+      initdb --locale=C --encoding=UTF8
+      POSTGRES_RUN_INITIAL_SCRIPT="true"
+      echo
+      echo "PostgreSQL initdb process complete."
+      echo
     fi
 
-    XDG_DATA_HOME=$DEVSHELL_STATE_DIR/data XDG_CONFIG_HOME=$DEVSHELL_STATE_DIR/config exec ${pkgs.caddy}/bin/caddy run --config ${caddyFile} --adapter caddyfile
+    cp ${postgresConf} "$PGDATA/postgresql.conf"
+
+    if [[ "$POSTGRES_RUN_INITIAL_SCRIPT" = "true" ]]; then
+      echo
+      echo "PostgreSQL is setting up the initial database."
+      echo
+      OLDPGHOST="$PGHOST"
+      PGHOST=$(mktemp -d "$DEVSHELL_STATE_DIR/pg-init-XXXXXX")
+
+      function remove_tmp_pg_init_sock_dir() {
+        if [[ -d "$1" ]]; then
+          rm -rf "$1"
+        fi
+      }
+      trap "remove_tmp_pg_init_sock_dir '$PGHOST'" EXIT
+
+      pg_ctl -D "$PGDATA" -w start -o "-c unix_socket_directories=$PGHOST -c listen_addresses= -p 5432"
+      ${setupInitialDatabases}
+
+      pg_ctl -D "$PGDATA" -m fast -w stop
+      remove_tmp_pg_init_sock_dir "$PGHOST"
+      PGHOST="$OLDPGHOST"
+      unset OLDPGHOST
+    fi
+    unset POSTGRES_RUN_INITIAL_SCRIPT
   '';
+
+  startPostgresql = pkgs.writeShellScript "start-postgres.sh" ''
+    set -euo pipefail
+    ${setupPostgres}/bin/setup-postgres
+    exec ${pkgs.postgresql_16}/bin/postgres
+  '';
+
+  procFile = pkgs.writeTextFile {
+    name = "Procfile";
+    text = ''
+      caddy: exec ${startCaddy}
+      php-fpm: exec ${startPhpFpm}
+      postgresql: exec ${startPostgresql}
+      npm-watch: npm run watch
+    '';
+  };
 
   # Had to wrap overmind so that I could set the root directory. It defaults to the dir where the procfile is located
   # and there's no env var to override this dir value, for some reason.
@@ -88,15 +168,6 @@ let
 
     OVERMIND_PROCFILE=${procFile} ${pkgs.overmind}/bin/overmind "$@" $extraParams
   '';
-
-  procFile = pkgs.writeTextFile {
-    name = "Procfile";
-    text = ''
-      caddy: exec ${startCaddy}
-      php-fpm: exec ${startPhpFpm}
-      npm-watch: npm run watch
-    '';
-  };
 in
 {
   default = pkgs.mkShell {
